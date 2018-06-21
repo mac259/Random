40998d486a254c7265f4f52800755ff49274f87d
Aniket Mukherjee <mukherjeeaniket007@gmail.com>
2018-06-21 22:50:23 +0530

    Local Pass

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': 'qwerty'
-        
+        'password': 'Pass@1234'
+
     },
 	'database': 'elective',
     'users_table': 'account'
