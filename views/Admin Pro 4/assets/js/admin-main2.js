893b86353b7539bb7df7112dd1e1e13f2a0623cd
Aniket Mukherjee <mukherjeeaniket007@gmail.com>
2018-07-31 22:58:53 +0530

    UI Enhancement
    Additional plugins + 2 new pages

--- views/Admin Pro 4/assets/js/admin-main2.js
+++ views/Admin Pro 4/assets/js/admin-main2.js
@@ -254,3 +254,17 @@ demo = {
   }
 
 }
+
+$('.datetimepicker').datetimepicker({
+    icons: {
+        time: "fa fa-clock-o",
+        date: "fa fa-calendar",
+        up: "fa fa-chevron-up",
+        down: "fa fa-chevron-down",
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-screenshot',
+        clear: 'fa fa-trash',
+        close: 'fa fa-remove'
+    }
+});
